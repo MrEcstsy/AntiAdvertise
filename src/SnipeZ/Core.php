9298abf0f4a9dd7cf2f27ecd8609e9bedb1cca3d
TheRealSnipeZ <57694923+TheRealSnipeZ@users.noreply.github.com>
2020-04-25 00:30:10 -0700

    Update Core.php

--- src/SnipeZ/Core.php
+++ src/SnipeZ/Core.php
@@ -1,29 +1,18 @@
 <?php
 
-declare(strict_types=1);
-
 namespace SnipeZ;
 
-use SnipeZ\Core;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\utils\TextFormat;
-class AntiSwearing implements Listener {
+use SnipeZ\AntiAdvertising;
+use SnipeZ\AntiSwearing;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+class Core extends PluginBase {
 
-    private $plugin, $badwords;
-    public function __construct(Core $plugin) {
-        $this->plugin = $plugin;
-        $this->badwords = ["anal", "anus", "ass", "bastard", "bitch", "boob", "cock", "cum", "cunt", "dick", "dildo", "dyke", "fag", "faggot", "fuck", "fuk", "handjob", "homo", "jizz", "cunt", "kike", "kunt", "muff", "nigger", "penis", "piss", "poop", "pussy", "queer", "rape", "semen", "sex", "shit", "slut", "titties", "twat", "vagina", "vulva", "wank", "FUCK", "BITCH", "FAGGOT", "DICK", "CUNT", "ASS"];
-    }
-    public function onChat(PlayerChatEvent $event) {
-        $msg = $event->getMessage();
-        $player = $event->getPlayer();
-        foreach ($this->badwords as $badwords) {
-            if (strpos($msg, $badwords) !== false) {
-                $player->sendMessage(TextFormat::RED . "§l§9Anti-Core > §6Please don't Swear on our server.");
-                $event->setCancelled();
-                return;
-            }
-        }
+    const PERM_RANK = C::BOLD . C::BLUE . "Permission> " . C::RESET . C::GRAY . "You Need A Rank To Access This Command";
+    const PERM_STAFF = C::BOLD . C::BLUE . "Permission> " . C::RESET . C::GRAY . "Only Staff Can Use This Command";
+    const USE_IN_GAME = C::BOLD . C::BLUE . "Command> " . C::RESET . C::GRAY . "Use Command In Game";
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(($this->antiadvertising = new AntiAdvertising($this)), $this);
+        $this->getServer()->getPluginManager()->registerEvents(($this->antiswearing = new AntiSwearing($this)), $this);
     }
 }
